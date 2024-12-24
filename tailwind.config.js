6616713e6089b7693ceec6062289a60d29934232
Daniel Li <inflmts@gmail.com>
2024-12-23 22:02:11 -0500

    Drop all the space

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
-const tile = `
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 28">
-  <path fill="none" stroke="white" opacity="0.2" d="M0,0 H16 L0,28 H16 Z M0,14 H16"/>
-</svg>`;
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['index.html', 'src/main.js'],
+  content: ['index.html', 'src/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        display: ['Barlow', ...defaultTheme.fontFamily.sans]
-      },
-      backgroundImage: {
-        // base64 is actually shorter than encodeURIComponent
-        'tile': `url('data:image/svg+xml;base64,${btoa(tile)}')`
+        sans: ['Barlow', ...defaultTheme.fontFamily.sans]
       }
     }
   },
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import tailwindcss from 'tailwindcss';
 import icon from './lib/icon.js';
-import template from './lib/template.js';
 
 /** @type {import('vite').UserConfig} */
 export default {
-  base: '',
   appType: 'mpa',
   plugins: [
-    template(),
     icon()
   ],
   css: {
